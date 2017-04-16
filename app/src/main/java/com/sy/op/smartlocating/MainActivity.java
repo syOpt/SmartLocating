db8f800087de5ea11f38aeb4662cd6f39528befe
syOpt <xzcmz828@126.com>
2017-04-16 16:57:48 +0800

    Playing audio feature added
    Now by clicking 'start' button, recording as well as playing can be done
    simultaneously.

--- app/src/main/java/com/sy/op/smartlocating/MainActivity.java
+++ app/src/main/java/com/sy/op/smartlocating/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sy.op.smartlocating;
 
+import android.media.AudioTrack;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +11,28 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 
+import static android.media.AudioManager.ERROR_DEAD_OBJECT;
+import static android.media.AudioManager.STREAM_MUSIC;
+import static android.media.AudioRecord.ERROR_BAD_VALUE;
 import static android.media.AudioRecord.RECORDSTATE_RECORDING;
 import static android.media.AudioRecord.STATE_UNINITIALIZED;
+import static android.media.AudioTrack.ERROR;
+import static android.media.AudioTrack.ERROR_INVALID_OPERATION;
 
 public class MainActivity extends AppCompatActivity {
-    private audioBuffer buff = new audioBuffer();
-    private audioBufferHandler buffHdl = new audioBufferHandler();
-    private int fs = 44100;
-    private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
-    private int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
-    private int bufferSize = AudioRecord.getMinBufferSize(fs, channelConfig, audioEncoding);
-    private int bufferReadRes;
-    private short[] buffer = new short[bufferSize];
+    private recAudioBuffer recBuff = new recAudioBuffer();
+    private plyAudioBuffer plyBuff = new plyAudioBuffer();
+    private recAudioBufferHandler recBuffHdl = new recAudioBufferHandler();
+    private int recFs = 44100, plyFs = 44100;
+    private int recChannelConfig = AudioFormat.CHANNEL_IN_MONO, plyChannelConfig = AudioFormat.CHANNEL_OUT_MONO;
+    private int recAudioEncoding = AudioFormat.ENCODING_PCM_16BIT, plyAudioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+    private int recBufferSize = AudioRecord.getMinBufferSize(recFs, recChannelConfig, recAudioEncoding);
+    private int plyBufferSize = AudioTrack.getMinBufferSize(plyFs, plyChannelConfig, plyAudioEncoding);
+    private int recBufferReadRes, plyBufferWriteRes;
+    private short[] recBuffer = new short[recBufferSize];
+    private short[] plyBuffer = new short[plyBufferSize * 10];
     private AudioRecord recorder;
+    private AudioTrack player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,61 +50,114 @@ public class MainActivity extends AppCompatActivity {
     public void stopMeasuring(View v) {
         findViewById(R.id.btnStart).setEnabled(true);
         findViewById(R.id.btnStop).setEnabled(false);
-        Thread stp = new Thread(buff);
-        stp.setName("stp");
-        stp.start();
+        Thread recStp = new Thread(recBuff);
+        recStp.setName("recStp");
+        recStp.start();
+        Thread plyStp = new Thread(plyBuff);
+        plyStp.setName("plyStp");
+        plyStp.start();
     }
 
 
     private void playAudio() {
-
+        Thread plyr = new Thread(plyBuff);
+        plyr.setName("ply");
+        plyr.start();
     }
 
     private void recAnalyse() {
-        Thread ana = new Thread(buff);
+        Thread ana = new Thread(recBuff);
         ana.setName("ana");
         ana.start();
     }
 
-    protected class audioBufferHandler extends Handler {
-        public audioBufferHandler() {}
+    protected class recAudioBufferHandler extends Handler {
+        public recAudioBufferHandler() {}
 
         @Override
         public void handleMessage(Message msg) {
+            // update distance value
             TextView xVal = (TextView)findViewById(R.id.txvX);
             xVal.setText(String.valueOf(msg.arg1));
         }
     }
 
-    protected class audioBuffer implements Runnable {
-
+    protected class plyAudioBuffer implements Runnable {
 
         @Override
         public void run() {
-
-
             switch (Thread.currentThread().getName()) {
-                case "ana":
-                    bufferReadRes = 0;
-                    recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, fs, channelConfig, audioEncoding, bufferSize * 2);
-                    recorder.startRecording();
-                    if (recorder.getState() == STATE_UNINITIALIZED)
-                        throw new RuntimeException("Failed to initialize recorder.");
-                    else if (recorder.getRecordingState() != RECORDSTATE_RECORDING)
-                        throw new RuntimeException("Failed to start recording.");
-                    while (bufferReadRes >= 0) {
-                        bufferReadRes = recorder.read(buffer, 0, 100);//bufferSize);
-                        int avg = 0;
-                        for (int i = 0; i < bufferReadRes; ++i) {
-                            avg += buffer[i];
+                case "ply":
+                    try {
+                        // initialize player
+                        player = new AudioTrack(STREAM_MUSIC, plyFs, plyChannelConfig, plyAudioEncoding, plyBufferSize, AudioTrack.MODE_STREAM);
+                        if (player.getState() == STATE_UNINITIALIZED)
+                            throw new RuntimeException("Failed to initialize player.");
+
+                        // generate audio data, x = A*sin(2*pi*f*t) -> xi = A*sin(2*pi*f*(1/plyFs*i))
+                        double A = 500, f = 15000;
+                        for (int i = 0; i < plyBufferSize * 10; ++i) {
+                            plyBuffer[i] = (short) (A * Math.sin(2.0 * Math.PI * f * (1.0 / (double)plyFs * (double)i)));
                         }
-                        if (bufferReadRes != 0) avg /= bufferReadRes;
-                        Message msg = new Message();
-                        msg.arg1 = avg;
-                        MainActivity.this.buffHdl.sendMessage(msg);
+
+                        // start playing
+                        plyBufferWriteRes = player.write(plyBuffer, 0, plyBufferSize * 10);
+                        player.play();
+                        while (plyBufferSize >= 0) {
+                            plyBufferWriteRes = player.write(plyBuffer, 0, plyBufferSize * 10);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                     break;
-                case "stp":
+                case "plyStp":
+                    try {
+                        player.stop();
+                        player.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Unknown handling thread met in Player.");
+            }
+        }
+    }
+
+    protected class recAudioBuffer implements Runnable {
+
+        @Override
+        public void run() {
+            switch (Thread.currentThread().getName()) {
+                case "ana":
+                    // initialize recorder
+                    recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, recFs, recChannelConfig, recAudioEncoding, recBufferSize * 2);
+                    if (recorder.getState() == STATE_UNINITIALIZED)
+                        throw new RuntimeException("Failed to initialize recorder.");
+
+                    // start recording
+                    recorder.startRecording();
+                    if (recorder.getRecordingState() != RECORDSTATE_RECORDING)
+                        throw new RuntimeException("Failed to start recording.");
+
+                    // analyze
+                    recBufferReadRes = 0;
+                    while (recBufferReadRes >= 0) {
+                        recBufferReadRes = recorder.read(recBuffer, 0, recBufferSize / 10);
+                        // analyse data
+                        int avg = 0;
+                        for (int i = 0; i < recBufferReadRes; ++i) {
+                            avg += recBuffer[i];
+                        }
+                        if (recBufferReadRes != 0) avg /= recBufferReadRes;
+
+                        // send result to UI thread
+                        Message msg = new Message();
+                        msg.arg1 = avg;
+                        MainActivity.this.recBuffHdl.sendMessage(msg);
+                    }
+                    break;
+                case "recStp":
                     try {
                         recorder.stop();
                         recorder.release();
@@ -103,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
                 default:
-                    throw new RuntimeException("Unknown handling thread.");
+                    throw new RuntimeException("Unknown handling thread met in recorder.");
             }
         }
     }
